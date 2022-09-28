6ac8e92802ff94bda922c3becc5cccb43f07e0b7
barrionuevoje <barrionuevoje@gmail.com>
2022-09-28 18:10:43 -0300

    Pre entrega Final

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Cabecera from "./componets/Cabecera";
+import Busqueda from "./componets/Busqueda";
+import "bootstrap/dist/css/bootstrap.css";
+import{Link, BrowserRouter, Route, Routes} from "react-router-dom"; 
+import Biblioteca from "./componets/Biblioteca";
+import Listas from "./componets/Listas";
+import TopRanked from "./componets/TopRanked";
+import ItemListContainer from "./componets/ItemListContainer/ItemListcontainer";
+import CartWidget from "./componets/CartWidget.js/CartWidget";
+import Login from "./componets/Login";
 function App() {
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      
+        
+        <Cabecera titulo="Om Musica!"/>
+          <nav>
+            
+            <ul className="nav  bg-info text-center">
+              <CartWidget />
+              <li className='nav-item'><Link className="nav-link text-center" to="/biblioteca">Biblioteca</Link></li>
+              <li className='nav-item'><Link className="nav-link text-center" to="/listas" >Listas</Link></li>
+              <li className='nav-item'><Link className="nav-link text-center" to="/topranked">Top Ranked</Link></li>
+              <li className='nav-item'><Link className="nav-link text-center" to="/login">Login</Link></li>
+            </ul>
+
+          </nav>
+          <ItemListContainer  greeting='Discos Disponibles!' />
+        <Busqueda />
+      <main className="container">
+        <Routes>
+          <Route path="/biblioteca" element={<Biblioteca />}/>
+          <Route path="/listas" element={<Listas />}/>
+          <Route path="/topranked" element={<TopRanked />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>  
+      </main> 
+      
+    </BrowserRouter>
   );
 }
 
